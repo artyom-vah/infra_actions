3a34d414ac88c9476edcf3777b75e859f0561fb9
artyom-vah <114044516+artyom-vah@users.noreply.github.com>
2023-05-03 19:13:24 +0500

    7

--- infra_project/infra_project/urls.py
+++ infra_project/infra_project/urls.py
@@ -3,5 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('', include('infra_app.urls', namespace='infra_app')),
-    path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls),    
 ]
